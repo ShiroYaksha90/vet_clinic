13fc90dff66ddb273a487e1b6e48c8fae8245ea4
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-05-04 14:46:49 +0300

    Fix typo

--- queries.sql
+++ queries.sql
@@ -8,7 +8,7 @@ SELECT name from animals WHERE neutered = true AND escape_attempts < 3;
 
 SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
 
-SELECT name, escape_attempts FROM animals WHERE weight_kg < 10.5;
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 SELECT name from animals WHERE neutered=true;
 
