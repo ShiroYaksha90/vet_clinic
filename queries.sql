78d67682f117746abdbfa2a9cfda7eef82b8ed52
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-05-04 20:39:58 +0300

    Add new queries

--- queries.sql
+++ queries.sql
@@ -2,16 +2,65 @@
 
 SELECT * from animals WHERE name LIKE '%mon';
 
-SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
 SELECT name from animals WHERE neutered = true AND escape_attempts < 3;
 
 SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
 
-SELECT name, escape_attempts FROM animals WHERE weight_kg < 10.5;
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 SELECT name from animals WHERE neutered=true;
 
 SELECT * from animals WHERE name <> 'Gabumon';
 
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+--- First transaction
+
+BEGIN;
+
+UPDATE animals SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+--- Second transaction
+
+BEGIN;
+UPDATE animals SET species = 'digmon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT species FROM animals;
+
+--- Third transaction
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+--- Fourth transaction
+
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > 'Jan 1, 2022';
+SAVEPOINT SAVE1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVE1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT * FROM animals;
+COMMIT;
+
+--- Queries
+SELECT COUNT(*) AS "Number of Animals" FROM animals;
+SELECT COUNT(*) AS "Number of Animals with no escape record" FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= 'Jan 1, 1990' AND date_of_birth <= 'Dec 31, 2000' GROUP BY species;
