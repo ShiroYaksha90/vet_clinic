05c22fe35ca187294d0e82fa18fb3d773e95b7aa
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-05-06 19:26:15 +0300

    Insert owners, species data Update animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 /* Populate database with sample data. */
-
+--- animals data
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (1, 'Agumon', '02/03/20', 0,true,10.23);
 
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (2, 'Gabumon', '11/15/18', 2,true,8);
@@ -21,3 +21,37 @@ INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_k
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (10, 'Blossom', '10/13/98', 3, true, 17);
 
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (11, 'Ditto', '05/14/22', 4, true, 22);
+
+--- owners data
+
+insert into owners (full_name, age) values ('Sam Smith', 34);
+
+insert into owners (full_name, age) values ('Jennifer Orwell', 19);
+
+insert into owners (full_name, age) values ('Bob', 45);
+
+insert into owners (full_name, age) values ('Melody Pond', 77);
+
+insert into owners (full_name, age) values ('Dean Winchester', 14);
+
+insert into owners (full_name, age) values ('Jodie Whittaker', 38);
+
+--- species data
+
+insert into species (name)  values ('Pokemon');
+
+insert into species (name)  values ('Digimon');
+
+--- update data
+
+UPDATE animals SET species_id =
+CASE WHEN name LIKE '%mon' then (SELECT id FROM species WHERE name = 'Digimon')
+ELSE (SELECT id FROM species WHERE name = 'Pokemon') END;
+
+UPDATE animals SET owner_id = CASE
+    WHEN name = 'Agumon' then (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+    WHEN name = 'Gabumon' OR name = 'Pikachu' then (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+    WHEN name = 'Devimon' OR name = 'Plantmon' then (SELECT id FROM owners WHERE full_name = 'Bob')
+    WHEN name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom' then (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+    WHEN name = 'Boarmon' OR name = 'Angemon' then (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+END;
