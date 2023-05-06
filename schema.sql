f851d266c1e643859136f6e616ba95ba8c1edb56
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-05-06 19:27:21 +0300

    Create owners, species tables Alter animals table

--- schema.sql
+++ schema.sql
@@ -10,3 +10,31 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species varchar(50);
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY, 
+    full_name VARCHAR(100),
+    age INT,
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY, 
+    name VARCHAR(50),
+    PRIMARY KEY(id)
+);
+
+
+
+ALTER TABLE animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+
+ALTER TABLE animals ADD FOREIGN KEY(species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+
+ALTER TABLE animals ADD FOREIGN KEY(owner_id) REFERENCES owners(id);
